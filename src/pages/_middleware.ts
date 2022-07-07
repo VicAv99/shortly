11ea2a3c5d59da2250bd999033f3da6933a21fed
Victor Avila <vavilamo@cisco.com>
2022-07-07 02:34:36 -0700

    fix: base pathname issue

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -1,9 +1,13 @@
 import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
 
 export async function middleware(req: NextRequest, ev: NextFetchEvent) {
-  if (req.nextUrl.pathname.startsWith("/api/get-url/")) return;
-  const slug = req.nextUrl.pathname.split("/").pop();
-  const url = `${req.nextUrl.origin}/api/get-url/${slug}`;
+  const pathname = req.nextUrl.pathname;
+
+  if (pathname === "/favicon.ico" || pathname === "/" || !pathname) return;
+  if (pathname.startsWith("/api/get-url/")) return;
+
+  const slug = pathname.split("/").pop();
+  const url = `${origin}/api/get-url/${slug}`;
   const data = await (await fetch(url)).json();
 
   if (data.url) {
--- src/pages/api/get-url/[slug].ts
+++ src/pages/api/get-url/[slug].ts
@@ -5,7 +5,7 @@ const slug = async (req: NextApiRequest, res: NextApiResponse) => {
   const { slug } = req.query;
 
   if (!slug || typeof slug !== "string") {
-    res.status(404).json({ error: "Please use with a slug" });
+    res.status(404).json({ message: "Please use with a slug" });
     return;
   }
 
@@ -17,7 +17,7 @@ const slug = async (req: NextApiRequest, res: NextApiResponse) => {
     res.setHeader("Content-Type", "application/json");
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Cache-Control", "s-maxage=10000, max-age=0");
-    res.status(404).json({ error: "Slug not found" });
+    res.status(404).json({ message: "Slug not found" });
     return;
   }
 
