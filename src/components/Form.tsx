d1824b32f0039265b96d5d63200dd329bf2c1ae2
Victor Avila <vavilamo@cisco.com>
2022-07-07 15:20:45 -0700

    feat: display error

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@mantine/core';
+import { Button, Notification } from '@mantine/core';
 import { useDebouncedValue } from '@mantine/hooks';
 import React from 'react';
 import { Control, useForm, ValidationMode } from 'react-hook-form';
@@ -25,6 +25,11 @@ export const ShortlyForm = () => {
   const { formState, ...form } = useForm<FormData>(formOpts);
   const { mutate, ...short } = trpc.useMutation(['shortly.create-short-link']);
   const onSubmit = (data: FormData) => mutate(data);
+  const [notification, setNotification] = React.useState(short.isError);
+  const clearError = () => {
+    short.reset();
+    setNotification(false);
+  };
 
   const slugValue = form.watch('slug');
   const [debounced] = useDebouncedValue(slugValue, 400);
@@ -34,46 +39,56 @@ export const ShortlyForm = () => {
   ]);
 
   return (
-    <form onSubmit={form.handleSubmit(onSubmit)} className='w-5/12'>
-      <ShortlyField
-        name='slug'
-        control={form.control as Control<any, any>}
-        inputProps={{
-          placeholder: 'Choose an awesome slug!',
-          rightSection: (
-            <ShortlySlugFieldIcon
-              control={form.control}
-              fetchLoading={isLoading}
-              isAvailable={data?.isAvailable}
-            />
-          ),
-          size: 'lg',
-        }}
-      />
-      <ShortlyField
-        name='url'
-        control={form.control as Control<any, any>}
-        inputProps={{
-          placeholder: 'Enter your long url!',
-          size: 'lg',
-        }}
-      />
-      <Button
-        disabled={isLoading || !data?.isAvailable || !formState.isValid}
-        type='submit'
-        variant='subtle'
-        fullWidth
-        mb={short.data?.id ? 250 : 300}
-        radius='md'
-        size='md'
-        loading={short.isLoading}
-      >
-        Generate Short Link
-      </Button>
-      {short.isError && (
-        <div className='text-red-500'>{short.error.message}</div>
-      )}
-      <ShortlyUrl shortLink={short.data} />
-    </form>
+    <>
+      <form onSubmit={form.handleSubmit(onSubmit)} className='w-5/12'>
+        <ShortlyField
+          name='slug'
+          control={form.control as Control<any, any>}
+          inputProps={{
+            placeholder: 'Choose an awesome slug!',
+            rightSection: (
+              <ShortlySlugFieldIcon
+                control={form.control}
+                fetchLoading={isLoading}
+                isAvailable={data?.isAvailable}
+              />
+            ),
+            size: 'lg',
+          }}
+        />
+        <ShortlyField
+          name='url'
+          control={form.control as Control<any, any>}
+          inputProps={{
+            placeholder: 'Enter your long url!',
+            size: 'lg',
+          }}
+        />
+        <Button
+          disabled={isLoading || !data?.isAvailable || !formState.isValid}
+          type='submit'
+          variant='subtle'
+          fullWidth
+          mb={short.data?.id ? 250 : 300}
+          radius='md'
+          size='md'
+          loading={short.isLoading}
+        >
+          Generate Short Link
+        </Button>
+        <ShortlyUrl shortLink={short.data} />
+        {(notification || short.isError) && (
+          <Notification
+            onClose={clearError}
+            color='red'
+            radius='md'
+            title='Something Went Wrong'
+            className='-mt-20'
+          >
+            {short.error?.message}
+          </Notification>
+        )}
+      </form>
+    </>
   );
 };
